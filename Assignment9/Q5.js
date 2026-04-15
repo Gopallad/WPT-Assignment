024759ac8c9ba2c211ef471d1aa51defc5908975
Gopal Narayan Lad <gopallad555@gmail.com>
2026-04-16 00:05:32 +0530

    Add files via upload

---
+++ Assignment9/Q5.js
@@ -0,0 +1,16 @@
+// let d="Welcome to Node JSON"
+// undefined
+//  console.log(d);
+// Welcome to Node JSON
+// undefined
+//  12+23
+// 35
+//  12+76
+// 88
+//  function add(a,f){ return (a+f)}
+// undefined
+//  add(23+76)
+// NaN
+// 65+9
+// 74
+//  save
