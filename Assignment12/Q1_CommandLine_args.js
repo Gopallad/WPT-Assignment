ad01984d12764375b5fde4e043dd9a040ba83a0d
Gopal Narayan Lad <gopallad555@gmail.com>
2026-04-22 00:15:21 +0530

    Add files via upload

---
+++ Assignment12/Q1_CommandLine_args.js
@@ -0,0 +1,13 @@
+// 1) accept command line args in node js 
+//    and display addition of even numbers from that
+import process from 'process';
+
+console.log(process.argv);
+const a=parseInt(process.argv[3]);
+const b=parseInt(process.argv[4]);
+
+console.log(a+b);
+
+
+
+
